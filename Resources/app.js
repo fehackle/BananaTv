fba9f0ca20ac2972188314cecadcd915a3e004a9
chaqui <josue.chaqui@gmail.com>
2014-05-01 19:29:15 -0600

    arreglando mas cosas

--- Resources/app.js
+++ Resources/app.js
@@ -1,11 +1,10 @@
-(function(){
-	var win = Ti.UI.createWindow({
+var win = Ti.UI.createWindow({
 	backgroundColor: "white"
 });
 url ="http://apibananatv-chaqui.rhcloud.com/?canal=1";
 xhr = Ti.Network.createHTTPClient({
 	onload:function(x){
-		loadJson(JSON.parse(this.response));
+		alert(this.responseText);
 	},
 	onerror:function(x){},
 	onsendstream:function(x){},
@@ -13,8 +12,24 @@ xhr = Ti.Network.createHTTPClient({
 	onreadystatechange:function(x){},
 	timeout:5000	
 });
-xhr.open('GET',url);
-xhr.send();
-function loadJson(json){
-	var table = Ti.UI.createTableView();
+var winPrincipal = Ti.UI.createWindow({
+	backgroundColor:"White",
 });
+var labelGuatevision = Ti.UI.createLabel({
+  color: '#900',
+  font: { fontSize:16},
+  shadowColor: '#aaa',
+  shadowOffset: {x:5, y:5},
+  shadowRadius: 3,
+  text: 'Guatevision',
+  textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+  top: 30,
+  width: Ti.UI.SIZE, height: Ti.UI.SIZE
+});
+winPrincipal.add(labelGuatevision);
+function iniciar(){
+	xhr.open('GET',url);
+	xhr.send();
+	winPrincipal.open();
+}
+iniciar();
