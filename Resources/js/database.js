8c59145f1e2a22b5bded8313cd93f8315a9e56c5
chaqui <josue.chaqui@gmail.com>
2014-05-12 16:19:54 -0600

    reconfigurando conexion a bd

--- Resources/js/database.js
+++ Resources/js/database.js
@@ -1,29 +1,20 @@
-function database(request,tabla,nombre,id,hora){
+function database(request,tabla,nombre,id,hora, idCanal){
+	Ti.Database.install('bds/appBananaView.sqlite','appBananaView');
 	switch(request){
-		case 'create':{
-			try{
-			var db = Ti.Database.open("canales");
-			db.execute("DROP TABLE IF EXISTS 'canal'; DROP TABLE IF EXISTS 'programa'; CREATE  TABLE IF NOT EXISTS `canal` ( `idcanales` INT NOT NULL ,  `nombre` VARCHAR(45) NULL ,  PRIMARY KEY (`idcanales`) ) ENGINE = InnoDB; CREATE  TABLE IF NOT EXISTS `programa` ( `idprogramas` INT NOT NULL , `nombre` VARCHAR(60) NULL, `horaDeInicio` VARCHAR(60) NULL , `canales_idcanales` INT NOT NULL , PRIMARY KEY (`idprogrmas`, `canales_idcanales`) , INDEX `fk_progrmas_canales` (`canales_idcanales` ASC) , CONSTRAINT `fk_progrmas_canales` FOREIGN KEY (`canales_idcanales` ) REFERENCES `canal` (`idcanales` )");
-			alert ("bd creada");
-			db.close();
-			}
-			catch(e){
-				alert("error");
-			}
-			}
 		case 'insert':{
 			switch(tabla){
 				case "canal":{
-					var db = Ti.Database.open("canales");
-					db.execute("INSERT INTO canal VALUES("+id+",'"+nombre+"')");
+					var db = Ti.Database.open("appBananaView");
+					
+					db.execute("INSERT INTO canal VALUES("+id+",'"+nombre+"');");
 					alert ("bd insertado canal");
 					db.close();
 					break;	
 				}
 				
 				case "programa":{
-					var db = Ti.Database.open("canales");
-					db.execute("INSERT INTO programa VALUES("+id+",'"+nombre+"','"+hora+"')");
+					var db = Ti.Database.open("appBananaView");
+					db.execute("INSERT INTO programa VALUES("+id+",'"+nombre+"','"+hora+"',"+idCanal+");");
 					alert ("bd insertado programa");
 					db.close();
 					break;
@@ -33,16 +24,33 @@ function database(request,tabla,nombre,id,hora){
 		case 'select':{
 			switch(tabla){
 				case "canal":{
-					var db = Ti.Database.open("canales");
-					var resultado =db.execute("SELECT * FROM canal");
+					var db = Ti.Database.open("appBananaView");
+					var setResultado =db.execute("SELECT nombre, idcanales  FROM canal");
+					var resultado = new Array();
+					while (resultSet.isValidRow()){
+						var fila = new Object();
+						fila["nombre"]=setResultado.fieldByName("nombre");
+						fila["id"]=setResultado.fieldByName("idcanales");
+						resultado[resultado.length]=fila;
+						setResultado.next();	
+					}
 					db.close();
 					return resultado;
 					break;	
 				}
 				
 				case "programa":{
-					var db = Ti.Database.open("canales");
-					var resultado =db.execute("SELECT * FROM programa");
+					var db = Ti.Database.open("appBananaView");
+					var setResultado =db.execute("SELECT nombre, horaDeInicio FROM programa WHERE canales_idcanales = "+String.toString(id));
+					var resultado = new Array();
+					while (resultSet.isValidRow()){
+						var fila = new Object();
+						fila["nombre"]=setResultado.fieldByName("nombre");
+						fila["horaDeInicio"]=setResultado.fieldByName("horaDeInicio");
+						resultado[resultado.length]=fila;
+						setResultado.next();	
+					}
+					setResultado.close();
 					db.close();
 					return resultado;
 					break;
